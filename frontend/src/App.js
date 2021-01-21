f17f2dc3f3bdcd2cfb26b0955def614ea8d2fdb7
chrissiemhrk <chrissiemhrk@gmail.com>
2021-01-21 13:42:52 +0200

    feat: create grid container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <div className="logo">
+          <a href="/">Fashion Hub</a>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="/cart">Cart</a>
+            </li>
+            <li>
+              <a href="/signin">Sign In</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <ul>
+          <li>one</li>
+          <li>one</li>
+          <li>one</li>
+          <li>one</li>
+          <li>one</li>
+        </ul>
+      </main>
+      <footer>
+        <p>All rights reserved 2021</p>
+      </footer>
     </div>
   );
 }
