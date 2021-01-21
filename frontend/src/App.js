3132bd6795dd313dffeb4b748421781dad2a7e81
chrissiemhrk <chrissiemhrk@gmail.com>
2021-01-21 14:59:06 +0200

    feat: add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,51 @@ function App() {
         </ul>
       </main>
       <footer>
-        <p>All rights reserved 2021</p>
+        <div className="footer-top">
+          <div className="logo">
+            <a href="/">FH</a>
+          </div>
+          <nav>
+            <ul className="shopping-category">
+              <li>
+                <a href="/women">Women</a>
+              </li>
+              <li>
+                <a href="/men">Men</a>
+              </li>
+              <li>
+                <a href="/accessories">Accessories</a>
+              </li>
+            </ul>
+            <ul className="contact">
+              <li>
+                <p>(000) 000 000 000</p>
+              </li>
+              <li>
+                <p>info@fashionhub.com</p>
+              </li>
+            </ul>
+          </nav>
+          <ul className="social-media">
+            <li>
+              <a href="facebook.com">Facebook</a>
+            </li>
+            <li>
+              <a href="instagram.com">Instagram</a>
+            </li>
+            <li>
+              <a href="twitter.com">Twitter</a>
+            </li>
+            <li>
+              <a href="pinterest.com">Pinterest</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-bottom">
+          <p>
+            Made by <a href="https://twitter.com/chrissiemhrk">Chrissie</a>
+          </p>
+        </div>
       </footer>
     </div>
   );
