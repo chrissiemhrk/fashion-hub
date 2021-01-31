ed30aed26873449748d22a0c9ec1e0731d4d4c8e
chrissiemhrk <chrissiemhrk@gmail.com>
2021-01-31 16:09:07 +0200

    feat: import header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,10 @@
 import "./App.scss";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="grid-container">
-      <header>
-        <div className="logo">
-          <a href="/">Fashion Hub</a>
-        </div>
-        <nav>
-          <ul className="shopping-category">
-            <li>
-              <a href="/women">Women</a>
-            </li>
-            <li>
-              <a href="/men">Men</a>
-            </li>
-            <li>
-              <a href="/accessories">Accessories</a>
-            </li>
-          </ul>
-          <ul className="cart-signin">
-            <li>
-              <a href="/cart">Cart</a>
-            </li>
-            <li>
-              <a href="/signin">Sign In</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
       <main>
         <ul>
           <li>one</li>
