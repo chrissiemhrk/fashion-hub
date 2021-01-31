d2d9b037e5e5c3d5c52e31b196ad255012db82ff
chrissiemhrk <chrissiemhrk@gmail.com>
2021-01-31 17:58:16 +0200

    feat: create footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,51 @@
+export default function Header() {
+  return (
+    <footer>
+      <div className="footer-top">
+        <div className="logo">
+          <a href="/">FH</a>
+        </div>
+        <nav>
+          <ul className="shopping-category">
+            <li>
+              <a href="/women">Women</a>
+            </li>
+            <li>
+              <a href="/men">Men</a>
+            </li>
+            <li>
+              <a href="/accessories">Accessories</a>
+            </li>
+          </ul>
+          <ul className="contact">
+            <li>
+              <p>(000) 000 000 000</p>
+            </li>
+            <li>
+              <p>info@fashionhub.com</p>
+            </li>
+          </ul>
+        </nav>
+        <ul className="social-media">
+          <li>
+            <a href="https://facebook.com">Facebook</a>
+          </li>
+          <li>
+            <a href="https://instagram.com">Instagram</a>
+          </li>
+          <li>
+            <a href="https://twitter.com">Twitter</a>
+          </li>
+          <li>
+            <a href="https://pinterest.com">Pinterest</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-bottom">
+        <p>
+          Made by <a href="https://twitter.com/chrissiemhrk">Chrissie</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
