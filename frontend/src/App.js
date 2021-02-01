4e245dbd4512b2bfcf26f07ed4082e49ddce72ce
chrissiemhrk <chrissiemhrk@gmail.com>
2021-02-01 14:50:08 +0200

    feat: import routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
 import "./App.scss";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import React from "react";
+import Routes from "./routes";
 
 export default function App() {
   return (
-    <div className="grid-container">
-      <Header />
-      <main>
-        <ul>
-          <li>one</li>
-          <li>one</li>
-          <li>one</li>
-          <li>one</li>
-          <li>one</li>
-        </ul>
-      </main>
-      <Footer />
+    <div>
+      <Routes />
     </div>
   );
 }
