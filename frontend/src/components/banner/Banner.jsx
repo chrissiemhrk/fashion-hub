186f8e458962a8f488a7852458796ffb34bdfb49
chrissiemhrk <chrissiemhrk@gmail.com>
2021-02-08 09:48:22 +0200

    feat: create banner component

---
+++ frontend/src/components/banner/Banner.jsx
@@ -0,0 +1,14 @@
+export default function Banner() {
+  return (
+    <section className="banner">
+      <p className="banner-text">Fashion Hub</p>
+      <p className="banner-text">Fashion Hub</p>
+      <p className="banner-text">Fashion Hub</p>
+      <p className="banner-text">Fashion Hub</p>
+      <p className="banner-text">Fashion Hub</p>
+      <p className="banner-text">Fashion Hub</p>
+      <p className="banner-text">Fashion Hub</p>
+      <p className="banner-text">Fashion Hub</p>
+    </section>
+  );
+}
